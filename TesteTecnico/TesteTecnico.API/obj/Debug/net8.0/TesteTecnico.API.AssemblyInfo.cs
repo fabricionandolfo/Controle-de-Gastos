e1fa08cc077f9f4bfa3f395aa17b1c4b1bc98cc1
Fabricio nandolfo ramos <42248827+fabricionandolfo@users.noreply.github.com>
2026-04-21 22:38:55 -0300

    .
    .

--- TesteTecnico/TesteTecnico.API/obj/Debug/net8.0/TesteTecnico.API.AssemblyInfo.cs
+++ TesteTecnico/TesteTecnico.API/obj/Debug/net8.0/TesteTecnico.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TesteTecnico.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3aad3f4090dee7b871453fe577a67bcfddc23c7b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d723910eb4723242c125dc751133069b5f83c81e")]
 [assembly: System.Reflection.AssemblyProductAttribute("TesteTecnico.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TesteTecnico.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
